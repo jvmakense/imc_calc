966dd0665714dcfe657fb2ff19ffedef179dd0be
joao vitor <joaomakenze@gmail.com>
2024-09-19 18:10:40 -0300

    IMC react

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import ImcCalc from './components/ImcCalc'
+import Button from './components/Button'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='container'>
+      <ImcCalc />
+      <Button />
+    </div>
+  )
+}
+
+export default App
